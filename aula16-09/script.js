45f5eb964b685ccfab12a3b4e55f53119e70532d
Daniel Teixeira <daniel.teixeira.corporativo@gmail.com>
2026-09-16 22:22:37 -0300

    feat: Introducao a popup e git pages

---
+++ aula16-09/script.js
@@ -0,0 +1,17 @@
+// var nomeUsuario = prompt("Digite seu nome: ")
+
+// console.log("Nome do usuário inserido: ", nomeUsuario)
+
+// var titulo = document.getElementById("titulo") //uma breve introdução ao DOM, um exemplo
+// titulo.textContent = `Olá, ${nomeUsuario}!`
+
+// alert("Seja bem vindo!")
+
+// Window.alert()
+
+// alert("Bem-vindo!")
+// var nome = prompt("Digite um nome: ")
+// alert("Nome digitado: " + nome)
+
+// var num = Number(prompt("Digite um número: ")) //parseInt ou parseFloat
+// alert(num + 10)
---
+++ aula16-09/scriptEx.js
@@ -0,0 +1,6 @@
+let primeiroNum = Number(prompt("Digite o primeiro número: "))
+let segundoNum = Number(prompt("Digite o segundo número: "))
+let soma = primeiroNum + segundoNum
+let produto = primeiroNum * segundoNum
+console.log(`A soma entre ${primeiroNum} + ${segundoNum} é ${soma}`)
+alert(`O produto entre ${primeiroNum} x ${segundoNum} é ${produto} `)
